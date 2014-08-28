5dd86bce1e8e981db280c2209cc633755afa2682
Find <Findxiaoxun@gmail.com>
2014-08-28 13:38:03 +0800

    能够显示三组数据了

--- src/small/find/SmartPellow.java
+++ src/small/find/SmartPellow.java
@@ -1,17 +1,33 @@
+/**
+ * Copyright (C) 2009 - 2013 SC 4ViewSoft SRL
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package small.find;
 
-import android.app.Activity;
-import android.os.Bundle;
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
 import org.achartengine.chart.PointStyle;
+import small.find.R;
 import org.achartengine.model.SeriesSelection;
 import org.achartengine.model.XYMultipleSeriesDataset;
 import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
+import android.app.Activity;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
@@ -20,8 +36,6 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class SmartPellow extends Activity {
-
-
   /** The main dataset that includes all the series that go into a chart. */
   private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
   /** The main renderer that includes all the renderers customizing a chart. */
@@ -40,7 +54,13 @@ public class SmartPellow extends Activity {
   private EditText mY;
   /** The chart view that displays the data. */
   private GraphicalView mChartView;
-
+  
+  
+  int[] colors = new int[] { Color.BLUE, Color.GREEN, Color.CYAN };
+  String[] titles = new String[] { "温度", "压力", "声音" };
+  PointStyle[] styles = new PointStyle[] { PointStyle.CIRCLE, PointStyle.DIAMOND,  
+	        PointStyle.TRIANGLE };
+  
   @Override
   protected void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
@@ -62,9 +82,9 @@ public class SmartPellow extends Activity {
     mCurrentRenderer = (XYSeriesRenderer) savedState.getSerializable("current_renderer");
   }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
     setContentView(R.layout.xy_chart);
 
     // the top part of the UI components for adding new data points
@@ -72,6 +92,9 @@ public class SmartPellow extends Activity {
     mY = (EditText) findViewById(R.id.yValue);
     mAdd = (Button) findViewById(R.id.add);
 
+    
+
+    
     // set some properties on the main renderer
     mRenderer.setApplyBackgroundColor(true);
     mRenderer.setBackgroundColor(Color.argb(100, 50, 50, 50));
@@ -83,15 +106,24 @@ public class SmartPellow extends Activity {
     mRenderer.setZoomButtonsVisible(true);
     mRenderer.setPointSize(5);
 
+    mRenderer.setXAxisMax(600);
+    mRenderer.setXAxisMin(0);
+    mRenderer.setYAxisMin(0); 
+    mRenderer.setYAxisMax(40);
+    mRenderer.setPanLimits(new double[]{0,10,0,40});
+    
     // the button that handles the new series of data creation
     mNewSeries = (Button) findViewById(R.id.new_series);
     mNewSeries.setOnClickListener(new View.OnClickListener() {
       public void onClick(View v) {
-        String seriesTitle = "Series " + (mDataset.getSeriesCount() + 1);
-        // create a new series of data
-        XYSeries series = new XYSeries(seriesTitle);
-        mDataset.addSeries(series);
-        mCurrentSeries = series;
+//        String seriesTitle = "Series " + (mDataset.getSeriesCount() + 1);
+//        // create a new series of data
+//        XYSeries series = new XYSeries(seriesTitle);
+//        mDataset.addSeries(series);
+//        mCurrentSeries = series;
+    	    //添加三组数据
+    	    XYSeries temperature=new XYSeries("温度");
+    	    mDataset.addSeries(temperature);
         // create a new renderer for the new series
         XYSeriesRenderer renderer = new XYSeriesRenderer();
         mRenderer.addSeriesRenderer(renderer);
@@ -99,10 +131,49 @@ public class SmartPellow extends Activity {
         renderer.setPointStyle(PointStyle.CIRCLE);
         renderer.setFillPoints(true);
         renderer.setDisplayChartValues(true);
-        renderer.setDisplayChartValuesDistance(10);
+        renderer.setDisplayChartValuesDistance(5);
         mCurrentRenderer = renderer;
         setSeriesWidgetsEnabled(true);
+//        mChartView.repaint();
+        
+        
+        XYSeries sound=new XYSeries("声音");
+        mDataset.addSeries(sound);
+        XYSeriesRenderer renderer2 = new XYSeriesRenderer();
+        mRenderer.addSeriesRenderer(renderer2);
+        // set some renderer properties
+        renderer2.setPointStyle(PointStyle.DIAMOND);
+        renderer2.setFillPoints(true);
+        renderer2.setDisplayChartValues(true);
+        renderer2.setDisplayChartValuesDistance(5);
+        mCurrentRenderer = renderer2;
+        setSeriesWidgetsEnabled(true);
+//        mChartView.repaint();
+        
+        
+        XYSeries pressure = new XYSeries("压力");
+        mDataset.addSeries(pressure);
+        XYSeriesRenderer renderer3 = new XYSeriesRenderer();
+        mRenderer.addSeriesRenderer(renderer3);
+        // set some renderer properties
+        renderer3.setPointStyle(PointStyle.TRIANGLE);
+        renderer3.setFillPoints(true);
+        renderer3.setDisplayChartValues(true);
+        renderer3.setDisplayChartValuesDistance(5);
+        mCurrentRenderer = renderer3;
+        setSeriesWidgetsEnabled(true);
+        
+        
+        pressure.add(0, 10);
+        pressure.add(20, 30);
+        temperature.add(0, 20);
+        temperature.add(5, 20);
+        sound.add(2, 20);
+        sound.add(5, 30);
         mChartView.repaint();
+        
+        
+        
       }
     });
 
