38f5106971ed4cec1129aeeb76c6e6c93d4a84cc
Find <Findxiaoxun@gmail.com>
2014-08-28 09:20:19 +0800

    SmartPellow

---
+++ src/small/find/SmartPellow.java
@@ -0,0 +1,15 @@
+package small.find;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SmartPellow extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
